8189a60b874159f0bb5af3163170ee1cd304dba5
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-06-25 16:43:20 -0300

    Estilos, scripts e view alterados

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,3 +8,60 @@ for (item of menuItems) {
         item.classList.add('active');
     }
 }
+
+// Paginação
+function paginate(selectedPage, totalPages) {
+    let pages = [], oldPage;
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+        const firstAndLastPage = currentPage == 1 || currentPage == 2 || currentPage == (totalPages - 1) || currentPage == totalPages;
+        const pagesBeforeSelectedPages = currentPage >= selectedPage - 1;
+        const pagesAfterSelectedPages = currentPage <= selectedPage + 1;
+
+        if (firstAndLastPage || pagesBeforeSelectedPages && pagesAfterSelectedPages) {
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push('...');
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1);
+            }
+
+            pages.push(currentPage);
+
+            oldPage = currentPage;
+        }
+    }
+
+    return pages;
+}
+
+function createPagination(pagination) {
+    const filter = pagination.dataset.filter;
+    // + transforma a string em número
+    const page = +pagination.dataset.page;
+    const total = +pagination.dataset.total;
+    const pages = paginate(page, total);
+
+    let elements = '';
+
+    for (let page of pages) {
+        if (String(page).includes('...')) {
+            elements += `<span>${page}</span>`;
+        } else {
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`;
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`;
+            }
+        }
+    }
+
+    pagination.innerHTML = elements;
+}
+
+const pagination = document.querySelector('.pagination');
+
+if (pagination) {
+    createPagination(pagination);
+}
